333e341740df11ac8784a7886f29e6dd0d8e8ceb
DESKTOP-3IVCKQQ\lkas7 <lkas78@naver.com>
2022-09-17 16:45:10 +0900

    Ch.1, Ch.2 시작

---
+++ Ch.2 JSX/hello-react/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Fragment } from 'react';
+
+function App() {
+  return (
+    // <Fragment>
+    //   <h1>Hello</h1>
+    // </Fragment>
+    <div>
+      <h1>Hello</h1>
+    </div>
+
+  );
+}
+
+export default App;
